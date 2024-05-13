9e189ca058c784de69e888acace467abbd9e7718
EzeanoroEbuka <165820779+EzeanoroEbuka@users.noreply.github.com>
2024-05-13 17:08:40 +0100

    second hour push

--- gateOne/Day seven/Java Code/PhoneBook.java
+++ gateOne/Day seven/Java Code/PhoneBook.java
@@ -2,19 +2,51 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class PhoneBook {
-public static void main(String... args) {
+ static Scanner scan = new Scanner(System.in);
+ ArrayList<String> nameAndNumberList = new ArrayList<>(); 
+	public static void main(String... args) {
 
-	 Scanner scanner  = new Scanner(System.in);
-	 ArrayList<String> nameAndNumberList = new ArrayList<>(); 
-	//nameAndNumber = addContacts();
+		afeezMenu();
+
+public static void afeezMenu() {
+	 //Scanner scanner  = new Scanner(System.in);
+	
+	System.out.println("""
+	:::::::::::::::::::::::
+	::  AFEEZ CONTACTS   ::
+	:::::::::::::::::::::::
+	
+	Welcome Afeez.....
+	Select from option (1 - 6)
+
+	1. Add Contact
+	2. Remove Contact
+	3. Find Contact by Phone Number
+	4. Find Contact by first Name
+	5. Find Contact by Last Name
+	6. Edit Contact
+	0. Back
+			""");
+	int navigate = scanner.nextInt();
+	switch(navigate) {
+	case 1 -> addContacts();
+	case 2 -> removeContact();
+	case 3 -> findByNumber();
+	case 4 -> findByFirstName();
+	case 5 -> findByLastName();
+	case 6 -> editContact();
+	case 0 -> afeezMenu();
+	}
+	// ArrayList<String> nameAndNumberList = new ArrayList<>(); 
+	
 	addContacts();
+	
 
-	anotherContacts();
 }
 
 public static void addContacts() {
 
-	Scanner scanner  = new Scanner(System.in);
+	//Scanner scanner  = new Scanner(System.in);
 	ArrayList<String> nameAndNumberList = new ArrayList<>();
 
 	System.out.println("""
@@ -30,11 +62,19 @@ public static void addContacts() {
 	System.out.println("Enter New Contact Telephone");
 	String contactNumber = scanner.nextLine();
 
+	if(contactNumber.length() == 11 && contactNumber.charAt(0) == '0'){
 	nameAndNumberList.add(contactNumber);
-	
+	System.out.println("Contact created successfully");
+	anotherContacts();
 
+	System.out.println(nameAndNumberList);
+	} 
+	else {
+		System.out.println("Incorrect Number");
+		
+		addContacts();
+	    }
 }
-
 public static void anotherContacts() {
 
 	Scanner scanner  = new Scanner(System.in);
@@ -44,9 +84,18 @@ public static void anotherContacts() {
 	if(addContactAgain.equalsIgnoreCase("Yes")){
 		addContacts();}
 	else{
-		System.out.println("Contact Added Successfully");
-	  }
-
-}
+		System.out.println("""
+			Contact Added Successfully
+			Enter (0) to Navigate to Menu
+			""");
+	  	int moveBack = scanner.nextInt();
+		switch(moveBack){
+		case 0 -> afeezMenu();
+		default  -> anotherContacts();
+		}
+	}
 	
+}
+
+//class	
 }
