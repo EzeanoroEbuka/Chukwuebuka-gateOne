da452a99f9c495eea9a578e19c974331d8c39347
EzeanoroEbuka <165820779+EzeanoroEbuka@users.noreply.github.com>
2024-05-13 19:14:08 +0100

    my third push

--- gateOne/Day seven/Java Code/PhoneBook.java
+++ gateOne/Day seven/Java Code/PhoneBook.java
@@ -2,15 +2,14 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class PhoneBook {
- static Scanner scan = new Scanner(System.in);
- ArrayList<String> nameAndNumberList = new ArrayList<>(); 
-	public static void main(String... args) {
+ static Scanner scanner = new Scanner(System.in);
+ static ArrayList<String> nameAndNumberList = new ArrayList<>(); 
+public static void main(String... args) {
 
 		afeezMenu();
-
+}
 public static void afeezMenu() {
-	 //Scanner scanner  = new Scanner(System.in);
-	
+		 	
 	System.out.println("""
 	:::::::::::::::::::::::
 	::  AFEEZ CONTACTS   ::
@@ -30,15 +29,14 @@ public static void afeezMenu() {
 	int navigate = scanner.nextInt();
 	switch(navigate) {
 	case 1 -> addContacts();
-	case 2 -> removeContact();
-	case 3 -> findByNumber();
-	case 4 -> findByFirstName();
-	case 5 -> findByLastName();
-	case 6 -> editContact();
+	//case 2 -> removeContact();
+	//case 3 -> findByNumber();
+	//case 4 -> findByFirstName();
+	//case 5 -> findByLastName();
+	//case 6 -> editContact();
 	case 0 -> afeezMenu();
 	}
-	// ArrayList<String> nameAndNumberList = new ArrayList<>(); 
-	
+		
 	addContacts();
 	
 
@@ -46,21 +44,19 @@ public static void afeezMenu() {
 
 public static void addContacts() {
 
-	//Scanner scanner  = new Scanner(System.in);
-	ArrayList<String> nameAndNumberList = new ArrayList<>();
-
 	System.out.println("""
 	:::::::::::::::::::::
 	::  ADD CONTACTS   ::
 	:::::::::::::::::::::
 			""");
+	scanner.nextLine();
 	System.out.println("Enter New Contact Name");
 	String contactName = scanner.nextLine();
-	
+	//scanner.next();
 	nameAndNumberList.add(contactName);
 
 	System.out.println("Enter New Contact Telephone");
-	String contactNumber = scanner.nextLine();
+	String contactNumber = scanner.next();
 
 	if(contactNumber.length() == 11 && contactNumber.charAt(0) == '0'){
 	nameAndNumberList.add(contactNumber);
@@ -76,25 +72,24 @@ public static void addContacts() {
 	    }
 }
 public static void anotherContacts() {
-
-	Scanner scanner  = new Scanner(System.in);
 	
+	Scanner scanner = new Scanner(System.in);
+
 	System.out.println("Add Another Contact? (Yes/No)");
 	String addContactAgain = scanner.nextLine();
 	if(addContactAgain.equalsIgnoreCase("Yes")){
 		addContacts();}
-	else{
+
+	if(addContactAgain.equalsIgnoreCase("No")){
 		System.out.println("""
 			Contact Added Successfully
-			Enter (0) to Navigate to Menu
 			""");
-	  	int moveBack = scanner.nextInt();
-		switch(moveBack){
-		case 0 -> afeezMenu();
-		default  -> anotherContacts();
-		}
-	}
-	
+		afeezMenu();}
+	else {
+		System.out.println("Invalid response");
+		anotherContacts();
+		
+	} 
 }
 
 //class	
