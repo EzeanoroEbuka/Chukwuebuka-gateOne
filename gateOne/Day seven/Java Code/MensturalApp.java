efde8235ff6779c70b749b7bdc862144d917e989
EzeanoroEbuka <165820779+EzeanoroEbuka@users.noreply.github.com>
2024-05-14 17:11:13 +0100

    Closing commit

--- gateOne/Day seven/Java Code/MensturalApp.java
+++ gateOne/Day seven/Java Code/MensturalApp.java
@@ -1,5 +1,42 @@
+import java.time.LocalDate;
 import java.util.Scanner;
-Public class MensturalApp {
+
+public class MensturalApp {
  public static void main(String... args) {
-  Scanner input = new Scanner(System.in);
+
+  	Scanner input = new Scanner(System.in);
+
+ 	LocalDate date = LocalDate.now();
+
+	int output = 0;  
+	int userYear = 0;
+	int currentYear = 0;
+	int currentMonth = 0;
+	int userMonth = 0;
+
+	System.out.println("Enter date when last period began");
+	String dateBeganPeriod = input.nextLine();
+
+	System.out.println("Enter date when last period stop");
+	String dateStopPeriod = input.nextLine();
+
+	date = date.replace("/","");
+
+	for(int count = date.length();count <= 0;count--) {  
+
+	 		int convertedInput = (input.charAt(count)) - '0';
+			userYear = (userYear * 10) + convertedInput;
+	}
+	System.out.println(date);
+
+	System.out.println(userYear  );
+
+
+
+
+
 	
+ }
+
+
+}
